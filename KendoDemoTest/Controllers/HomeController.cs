6622db3fd4ccf6f75eb3c5c0c9c4cb7a96a5f72d
tuanOts <anhtuanbtx1@gmail.com>
2022-10-24 23:15:27 +0700

    "BuildHome"

--- KendoDemoTest/Controllers/HomeController.cs
+++ KendoDemoTest/Controllers/HomeController.cs
@@ -33,5 +33,9 @@ namespace KendoDemoTest.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public ActionResult Tracking()
+        {
+            return PartialView("Tracking");
+        }
     }
 }
